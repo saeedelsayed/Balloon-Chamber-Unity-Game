ca68fbbc81b8994c960002263a04b55fa75d7835
Saeed Shamseldin <saidelsayed922@gmail.com>
2026-02-01 15:15:17 +0100

    Toe-in projection

--- Assets/StereoController.cs
+++ Assets/StereoController.cs
@@ -4,7 +4,9 @@ public class StereoController : MonoBehaviour
 {
     public Transform leftEye;
     public Transform rightEye;
+    public Transform convergencePoint;
     public float ipd = 0.064f;
+    public bool useToeIn = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,6 +21,17 @@ public class StereoController : MonoBehaviour
 
         if(rightEye != null)
         rightEye.localPosition = new Vector3(ipd / 2, 0, 0);
+
+        if(useToeIn && convergencePoint != null)
+        {
+            leftEye.LookAt(convergencePoint);
+            rightEye.LookAt(convergencePoint);
+        }
+        else
+        {
+            if(leftEye != null) leftEye.localRotation = Quaternion.identity;
+            if(rightEye != null) rightEye.localRotation = Quaternion.identity;
+        }
 
     }
 }
